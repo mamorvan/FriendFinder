4e89ad9b14e8c198f19a293d9e1a5ea5820681ae
Mary Morvan <mamorvan@yahoo.com>
2017-05-15 11:58:12 -0400

    basic routing done :hole:

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+var friendsArray = [
+	{
+		name: "Buffy",
+		photo: "https://vignette4.wikia.nocookie.net/vsbattles/images/8/85/Buffy-background.jpg/revision/latest?cb=20151127193857",
+		scores: ["1", "2", "3", "1", "1", "2", "1", "2", "2", "3"]
+	},
+	{
+		name: "Xander",
+		photo: "http://granades.com/wordpress/wp-content/uploads/2010/12/xander7.jpg",
+		scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+	},
+	{
+		name: "Willow",
+		photo: "http://vignette2.wikia.nocookie.net/buffy/images/e/ee/Willow_Season_7.jpg/revision/latest?cb=20141221135359",
+		scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+	},
+	{
+		name: "Faith",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/2/22/Faith_Lehane.jpg/220px-Faith_Lehane.jpg",
+		scores: ["3", "1", "2", "2", "3", "1", "2", "1", "2", "1"]
+	},
+	{
+		name: "Giles",
+		photo: "https://vignette2.wikia.nocookie.net/buffy/images/a/a3/Rupert_Giles.jpg/revision/latest?cb=20150118062913",
+		scores: ["5", "5", "1", "1", "5", "5", "3", "5", "5", "3"]
+	},
+	{
+		name: "Spike",
+		photo: "http://cdn4.gurl.com/wp-content/gallery/recurring-characters/spike-buffy.jpg",
+		scores: ["4", "3", "5", "5", "2", "3", "3", "5", "5", "5"]
+	},
+	{
+		name: "Angel",
+		photo: "https://upload.wikimedia.org/wikipedia/en/5/57/Angel_%28Buffy_the_Vampire_Slayer%29.jpg",
+		scores: ["4", "3", "5", "5", "2", "3", "3", "4", "4", "4"]
+	},
+	{
+		name: "Cordelia",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN77ZWoPOqPoR1d8_OfWIIW9Invu5zLBLiIM3nB8gaealgApUS",
+		scores: ["3", "4", "5", "5", "1", "3", "1", "3", "4", "4"]
+	},
+	{
+		name: "Tara",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQCsck185By5iWdDAOX5C-ceQg_M9ldvmO0Oh2qHVc57kNoEN",
+		scores: ["5", "5", "5", "2", "5", "5", "5", "3", "4", "3"]
+	}
+];
+
+module.exports = friendsArray;
