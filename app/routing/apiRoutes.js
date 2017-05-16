3ef28d36203825085815d183140ef478cbe0245a
Mary Morvan <mamorvan@yahoo.com>
2017-05-16 12:02:16 -0400

    matching logic done, work on modal display next :ant:

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,22 @@ module.exports = function(app) {
 	});
 
 	app.post("/api/friends", function(req, res){
+		var newResults = (req.body['scores']).map(Number);
+		var totalDifference = 100;
+		var friendIndex;
+		for (var i = 0; i < friends.length; i++){
+			var friendResults = friends[i]['scores'];
+			
+			var tempDifference = 0;
+			for (var j =0; j < newResults.length; j++){
+				tempDifference += Math.abs(newResults[j]-friendResults[j]);	
+			}
+			if (tempDifference < totalDifference) {
+				totalDifference = tempDifference;
+				friendIndex = i;
+			}
+		}
+		res.json(friends[friendIndex]);
 		friends.push(req.body);
-
 	});
 };
