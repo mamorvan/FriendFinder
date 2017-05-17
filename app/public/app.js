a849104d10f9e756954bd3d24b7b6d3e73a23d66
Mary Morvan <mamorvan@yahoo.com>
2017-05-17 13:04:47 -0400

    form validation done - very basic :pensive:

--- app/public/app.js
+++ app/public/app.js
@@ -22,9 +22,23 @@ $("#submit").on("click", function(event){
 			$("#question10").val()
 		]
 	};
+	if (newFriend.name === "" ){
+		alert("Please enter your name.  We need to know who you are to find you a match.");
+		return;
+	}
+	if (newFriend.photo === ""){
+		alert("Please give us a photo link.  We really need to know what you look like to find you a good coding buddy!");
+		return;
+	}
 
-	// for (var i = 0; newFriend.scores.length; )
-
+	//make sure all questions are answered
+	for (var i = 0; i < newFriend.scores.length; i++) {
+		if (newFriend.scores[i] === null) {
+			alert("Please fill in question " + (i + 1));
+			return;
+		}
+	};
+	//post new friend data, get match data back and insert into modal
 	$.post("/api/friends", newFriend, function(data){
 		$("#matchName").html(data.name);
 		$("#matchPhoto").attr("src", data.photo);
